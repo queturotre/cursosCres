f610dc3143b0756a2d33bfe44af6b5bd4467ddcd
Ricardo Miguel García <rich4rd3rd@gmail.com>
2022-09-12 16:24:51 -0500

    Merge
    Merging correctly to continue with 4 different components instead of 2.

--- src/app/components/createCourse/createCourse.component.ts
+++ src/app/components/createCourse/createCourse.component.ts
@@ -11,15 +11,6 @@ import { FormBuilder, FormGroup, Validators, } from '@angular/forms';
 export class CreateCourseComponent implements OnInit{
   @Output() sender: EventEmitter<any> = new EventEmitter();
     title: String = "Crear nuevo curso";
-<<<<<<< HEAD
-    studentData = [];
-    dataCurso = {
-      nrc: 0,
-      grado: 0,
-      curso: 0
-    }
-=======
->>>>>>> workablesf
 
     courseForm!: FormGroup;
 
@@ -30,7 +21,7 @@ export class CreateCourseComponent implements OnInit{
 
     ngOnInit(): void {
         this.buildForms();
-        this.getInitialData();
+        // this.getInitialData();
     }
 
     buildForms(){
@@ -53,51 +44,12 @@ export class CreateCourseComponent implements OnInit{
         });
     }
 
-<<<<<<< HEAD
-    getInitialData(){
-    //Traer asíncronamente la data de la url con ayuda de adminservice
-      this.adminService.getStudent().subscribe(
-        (resp) => {
-          let tree = resp;
-          this.studentData = tree;
-        },
-        (errResp) => {
-          console.error(errResp);
-        },
-        () => {
-          console.log('l');
-        })
-    }
-
-    saveCourseData(values: any){
-    console.log(this.studentData);
-    this.courseForm.markAllAsTouched();
-    if (this.courseForm.invalid) return;
-
-    let payload = {
-      nrc: values.NRC,
-      curso: values.curso,
-      grado: values.grado
-    }
-
-    // this.adminService.addCourse(payload).subscribe(
-    //   (resp) => {
-    //     if (resp.success){
-    //       console.log('Datos actualizados');
-    //     }
-    //   },
-    //   (errResp) => {
-    //     console.error(errResp);
-    //   }
-    // )
-=======
     saveCourseData(values: any){
       let payload = {
         nrc: values.NRC,
         grado: values.grado,
         curso: values.curso,
       }
->>>>>>> workablesf
     }
 }
   // this.courseForm.markAllAsTouched();
