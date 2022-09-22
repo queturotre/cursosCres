b110948db6e8d0ecca4311f2757b9b0cdf9d7df9
Ricardo Miguel García <rich4rd3rd@gmail.com>
2022-09-22 12:16:53 -0500

    I didn't save the changes last time, soon I'll retake from where I left off, I will add more functionalities.

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -10,11 +10,13 @@ export class AdminService {
   apiUrl: string;
 
   constructor(private http: HttpClient) {
+    // this.apiUrl = "http://localhost:3000";
     this.apiUrl = "https://my-json-server.typicode.com";
   }
 
   getCourseData(): Observable<any>{
     let url = this.apiUrl + "/queturotre/cursosCres/cursos";
+    // let url = this.apiUrl + "/courses";
     return this.http.get(url);
   }
 }
